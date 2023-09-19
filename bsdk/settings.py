4f62d5c1adcd7cfd2fb7772213412fa67d5bbd36
Malatesh  Sangamesh  Mantur <101890856+Malatesh810@users.noreply.github.com>
2023-09-19 22:06:25 +0530

    Update settings.py

--- bsdk/settings.py
+++ bsdk/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rushil.chirla@gmail.com'
+EMAIL_HOST_USER = 'manturmalatesh@gmail.com'
 EMAIL_HOST_PASSWORD = 'Iatb2020'
